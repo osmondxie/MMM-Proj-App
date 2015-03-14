5f7fbf25b6e9f1e5bd51f278dc38c51031e6dd01
Osmond Xie <funnydogo@hotmail.com>
2015-03-14 16:28:57 -0400

    Index Initial Addition

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+  <head>
+    <title> MMM Projects</title>
+  </head>
+  <body background="background.jpg">
+  
+  <h1>Login</h1>
+  
+  <form>
+  Username:<br>
+  <input type="text" name="username">
+  <br>
+  Password:<br>
+  <input type="text" name="password">
+  </form>
+  
+<?php
+  echo 2 + 2;
+?>
